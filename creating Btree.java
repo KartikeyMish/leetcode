415b76467bc922e28c9ad941c8f46f962e455408
Kartikey Mishra <76617485+KartikeyMish@users.noreply.github.com>
2023-06-22 00:35:55 +0530

    Create creating Btree.java

---
+++ creating Btree.java
@@ -0,0 +1,15 @@
+static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+        
+            return newNode;
+            
+        }
+    }
